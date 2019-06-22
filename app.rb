84fcef3e30e1231eaf6e58d4844a810c53dab1c1
Samuel T <ssamt1142@gmail.com>
2019-06-22 04:26:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    @operation.to_i
+    @number1.to_i @operation.to_i
   end
 
 end
